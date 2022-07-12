a99bf0f9bbe48c7957dcdf8776004513ede718a0
Suhas Wale <suhas.wale@atos.net>
2022-07-12 15:49:25 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,14 @@
+module "vpc" {
+    source  = "terraform-google-modules/network/google"
+    version = "~> 4.0"
+
+    project_id   = "var.project"
+    network_name = "suhas-network"
+    routing_mode = "GLOBAL"
+
 subnets = [
   {
-    subnet_name   = "gaurav-subnet"
+    subnet_name   = "suhas-subnet"
     subnet_ip     = "10.100.10.0/24"
     subnet_region = var.region
   }
