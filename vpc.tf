b98d3fa987e29b1b7817cf5720a03ac50f34bca5
Suhas Wale <suhas.wale@atos.net>
2022-07-12 16:22:27 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,7 @@ module "vpc" {
     source  = "terraform-google-modules/network/google"
     version = "~> 3.4.0"
 
-    project_id   = "var.project"
+    project_id   = var.project
     network_name = "suhas-network"
     routing_mode = "GLOBAL"
 
