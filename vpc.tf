505b733c2e8a14c01d0124d1ee9cabac04a6f954
Suhas Wale <suhas.wale@atos.net>
2022-07-12 16:34:14 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -8,7 +8,7 @@ module "vpc" {
 
 subnets = [
   {
-    subnet_name   = "suhas-subnet"
+    subnet_name   = "suhas-nested-subnet"
     subnet_ip     = "10.100.10.0/24"
     subnet_region = var.region
   }
